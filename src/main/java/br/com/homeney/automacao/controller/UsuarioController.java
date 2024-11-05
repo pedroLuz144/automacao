001d9f63e42a7c0a519bdbe11595f35f3b750b89
Pedro <pedro_luz@edu.unifil.br>
2024-11-05 18:14:05 -0300

    controllers

---
+++ src/main/java/br/com/homeney/automacao/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package br.com.homeney.automacao.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.homeney.automacao.dto.UsuarioDTO;
+import br.com.homeney.automacao.service.UsuarioService;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<UsuarioDTO> listarTodos() {
+        return usuarioService.listarTodos();
+    }
+
+    @PostMapping
+    public void inserir(@RequestBody UsuarioDTO usuario) {
+        usuarioService.Inserir(usuario);
+    }
+
+    @PutMapping
+    public UsuarioDTO alterar (UsuarioDTO usuario) {
+        return usuarioService.Alterar(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
+        usuarioService.Excluir(id);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/br/com/homeney/automacao/dto/UsuarioDTO.java
+++ src/main/java/br/com/homeney/automacao/dto/UsuarioDTO.java
@@ -16,6 +16,10 @@ public class UsuarioDTO {
         BeanUtils.copyProperties(usuario, this);
     }
 
+    public UsuarioDTO() {
+        
+    }
+
     public Long getId() {
         return id;
     }
